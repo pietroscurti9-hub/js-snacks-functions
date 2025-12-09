49010f2410eab50eca99030393bb53bbf8385a09
Pietro Scurti <pietroscurti9@gmail.com>
2025-12-09 18:16:18 +0100

    quarto esercizio completato

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,31 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function contavocali(stringa) {
+
+    let numeroVocali = 0
+
+    for (let i = 0; i < stringa.length; i++)
+
+        if (stringa[i] === "a" || 
+            stringa[i] === "e" || 
+            stringa[i] === "i" || 
+            stringa[i] === "o" || 
+            stringa[i] === "u") {
+
+            numeroVocali = numeroVocali + 1;
+
+        }
+    return numeroVocali
+
+}
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+let numeroVocali = contavocali(word)
 
+console.log(numeroVocali)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
